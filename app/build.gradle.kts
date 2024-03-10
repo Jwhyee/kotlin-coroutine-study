BoostStudy/design-patterns

각자 스터디용 모듈 추가

app/build.gradle.kts
@@ -1,0 +1,15 @@
+plugins {
+    id("design.patterns.study.kotlin-application-conventions")
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-text")
+    implementation(project(":audxo112"))
+    implementation(project(":ezhoon"))
+    implementation(project(":yangsooplus"))
+    implementation(project(":ldh019"))
+}
+
+application {
+    mainClass.set("design.patterns.study.app.AppKt")
+}

app/src/main/kotlin/design/patterns/study/app/App.kt
@@ -1,0 +1,10 @@
+/*
+ * This Kotlin source file was generated by the Gradle 'init' task.
+ */
+package design.patterns.study.app
+
+import org.apache.commons.text.WordUtils
+
+fun main() {
+    println("Hello World")
+}

app/src/test/kotlin/design/patterns/study/app/MessageUtilsTest.kt
@@ -1,0 +1,14 @@
+/*
+ * This Kotlin source file was generated by the Gradle "init" task.
+ */
+package design.patterns.study.app
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class MessageUtilsTest {
+    @Test fun testGetMessage() {
+        assertEquals("Hello      World!", MessageUtils.getMessage())
+    }
+}

ezhoon/build.gradle.kts
@@ -1,0 +1,3 @@
+plugins {
+    id("design.patterns.study.kotlin-library-conventions")
+}

ezhoon/src/test/kotlin/design/patterns/study/ezhoon/TestSample.kt
@@ -1,0 +1,17 @@
+/*
+ * This Kotlin source file was generated by the Gradle "init" task.
+ */
+package design.patterns.study.ezhoon
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class TestSample {
+    @Test
+    @DisplayName("샘플용")
+    fun testSample() {
+        val answer = 0
+        assertEquals(0, answer)
+    }
+}

Jwhyee/kotlin-coroutine-study

app/build.gradle.kts
@@ -1,0 +1,14 @@
+plugins {
+    id("kotlin.coroutine.study.kotlin-application-conventions")
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-text")
+    implementation(project(":jwhy"))
+    implementation(project(":ezhoon"))
+    implementation(project(":jaeeun"))
+}
+
+application {
+    mainClass.set("kotlin.coroutine.study.app.AppKt")
+}

app/src/main/kotlin/design/patterns/study/app/App.kt
@@ -1,0 +1,10 @@
+/*
+ * This Kotlin source file was generated by the Gradle 'init' task.
+ */
+package kotlin.coroutine.study.app
+
+import org.apache.commons.text.WordUtils
+
+fun main() {
+    println("Hello World")
+}

app/src/test/kotlin/design/patterns/study/app/MessageUtilsTest.kt
@@ -1,0 +1,14 @@
+/*
+ * This Kotlin source file was generated by the Gradle "init" task.
+ */
+package kotlin.coroutine.study.app
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class MessageUtilsTest {
+    @Test fun testGetMessage() {
+        assertEquals("Hello      World!", MessageUtils.getMessage())
+    }
+}

ezhoon/build.gradle.kts
@@ -1,0 +1,3 @@
+plugins {
+    id("kotlin.coroutine.study.kotlin-library-conventions")
+}

ezhoon/src/test/kotlin/design/patterns/study/ezhoon/TestSample.kt
@@ -1,0 +1,17 @@
+/*
+ * This Kotlin source file was generated by the Gradle "init" task.
+ */
+package kotlin.coroutine.study.ezhoon
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class TestSample {
+    @Test
+    @DisplayName("샘플용")
+    fun testSample() {
+        val answer = 0
+        assertEquals(0, answer)
+    }
+}